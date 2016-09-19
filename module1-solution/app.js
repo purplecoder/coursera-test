166cbf577aec598e556dacf268f24189a5a81ac1
purplecoder <andrey@razhanskiy.com>
2016-09-19 00:26:56 -0400

    Color added

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,10 +6,13 @@
   function LunchCheckController ($scope){
     $scope.dishes ="";
     $scope.checkDishes = function(){
-      if($scope.dishes == "")
+      if($scope.dishes == ""){
         $scope.message="Please enter data first";
+        $scope.message_color = "red";
+      }
       else {
         $scope.message=( $scope.dishes.split(",").length > 3) ? "Too much!" : "Enjoy!" ;
+        $scope.message_color = "green";
       }
     };
   }
