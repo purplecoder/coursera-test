d2b6f1eb381b022844be274668e413a39172a8d3
purplecoder <andrey@razhanskiy.com>
2016-09-19 00:40:58 -0400

    color and count

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -5,13 +5,20 @@
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController ($scope){
     $scope.dishes ="";
+    function count_non_empty(str){
+      var ret = 0;
+      angular.forEach(str.split(","),function(value,index){
+        ret += (value.trim() != "") ? 1 : 0;
+      });
+      return ret;
+    };
     $scope.checkDishes = function(){
       if($scope.dishes == ""){
         $scope.message="Please enter data first";
         $scope.message_color = "red";
       }
       else {
-        $scope.message=( $scope.dishes.split(",").length > 3) ? "Too much!" : "Enjoy!" ;
+        $scope.message=( count_non_empty($scope.dishes) > 3) ? "Too much!" : "Enjoy!" ;
         $scope.message_color = "green";
       }
     };
