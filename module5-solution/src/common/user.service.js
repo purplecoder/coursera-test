f5d183084154f57525610d84a030adb8c81306c3
Andrey <razhanskiy@gmail.com>
2016-11-03 18:53:40 -0400

    Doesn't delete cookie

--- module5-solution/src/common/user.service.js
+++ module5-solution/src/common/user.service.js
@@ -31,7 +31,7 @@ function UserService($q) {
   }
 
   function deleteCookie(name){
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   }
 
   service.saveUser = function (user) {
